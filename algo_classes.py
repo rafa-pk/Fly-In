e9a683df75c5f93dcdd5085a4f42a126d0a844a8
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2026-04-17 11:45:07 +0200

    Add files via upload

--- algo_classes.py
+++ algo_classes.py
@@ -1,3 +1,4 @@
+from typing import Any
 from dataclasses import dataclass 
 from graph_classes import Graph
 
@@ -15,36 +16,60 @@ class Drone:
 class HeapQueue:
 
     def __init__(self) -> None:
-        self.data = []
+        self._data = []
 
     def _sift_up(self, i: int) -> None:
         while i > 0:
             parent = (i - 1) // 2
-            if self.data[parent] <= self.data[i]:
+            if self._data[parent] <= self._data[i]:
                 break
-           self.data[parent], self.data[i] = self.data[i], self.data[parent]
-           i = parent 
+            self._data[parent], self._data[i] = \
+                self._data[i], self._data[parent]
+            i = parent 
 
     def _sift_down(self, i: int) -> None:
-        len = len(self.data)
+        length = len(self._data)
         while True:
             smallest = i
             left_child = 2 * i + 1
             right_child = 2 * i + 2
-            if left_child < len and self.data[left] < self.data[smallest]
+            if (left_child < length and
+               self._data[left_child] < self._data[smallest]):
+                smallest = left_child
+            if (right_child < length and
+               self._data[right_child] < self._data[smallest]):
+                smallest = right_child
+            if smallest == i:
+                break
+            self._data[i], self._data[smallest] = \
+                self._data[smallest], self._data[i]
+            i = smallest
 
     def push(self, value: int) -> None:
-        list.append(value)
-        self._sift_up(len(self.data) - 1)
+        if not value:
+            raise ValueError("Push needs a value, cannot push empty value")
+        self._data.append(value)
+        self._sift_up(len(self._data) - 1)
 
-    def pop(self, value: int) -> int:
-        if not self.data:
+    def pop(self) -> int:
+        if not self._data:
             raise ValueError("Attempt to pop from empty heap")
-        self.data[0], self.data[-1] = self.data[-1], self.data[0]
-        min = self.data.pop()
-        if self.data:
+        self._data[0], self._data[-1] = self._data[-1], self._data[0]
+        min = self._data.pop()
+        if self._data:
             self._sift_down(0)
         return min
+
+    def peek(self) -> int:
+        return self.data[0]
+
+    def heapify(self, array: list[Any]) -> None:
+        self._data = list(array)
+        for i in range(len(array) // 2 - 1, -1, -1):
+            self._sift_down(i)
+
+    def __repr__(self) -> str:
+        return f"HeapQueue({self._data})"
 
 
 class Utils:
